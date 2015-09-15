0623d574ca6020f490c31e9afea4738423f2f439
soilcurrency <soilcurrency@outlook.com>
2015-09-15 10:17:24 +0200

    NetworkId = 39338 (Testnet)

--- eth/protocol.go
+++ eth/protocol.go
@@ -30,7 +30,7 @@ var ProtocolVersions = []uint{61, 60}
 var ProtocolLengths = []uint64{9, 8}
 
 const (
-	NetworkId          = 1
+	NetworkId          = 39338
 	ProtocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
 )
 
